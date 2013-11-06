c3881b14e0e96301654c7f7b73393a94c7c60197
acrylian <wartgart@gmx.net>
2013-11-06 18:13:18 +0100

    Major rework to allow the selection of any gallery item as a featured image for Zenpage articles, categories and pages.

--- featured_image.php
+++ featured_image.php
@@ -1,15 +1,25 @@
 <?php
 /**
- * This plugin provides a functionality to assign an album thumbnail from any top level albums to an Zenpage news article as a "featured image"
- * You can use this image as an intro image to your article e.g. in the news loop. The benefit compared to the embedding an image 		   			
- * within the text of your article statically is that you can control the size of it using the object mode and Zenphoto's image processor
- * on your theme's layout dynamically.
+ * This plugin provides a functionality to assign an image from your albums to an Zenpage news article, category or page as a "featured image".
+ * You can use this image for example for headers of your single article/page/category pages or within the news article list as a thumbnail. 
+ * The benefit compared to the embedding an image within the text content statically is that you can control the size of it 
+ * via your theme's layout dynamically as with gallery items.
  *
- * Example usage on news.php either within the news loop or for single article display using the Zenphoto object model:
- * $thumb = getFeaturedImage($_zp_current_zenpage_news); 
- * if($thumb) {
- *  ?><img src="<?php echo pathurlencode($thumb->getThumb()); ?>" alt="<?php echo html_encode($thumb->getTitle()); ?>" /><?php
+ * To use it you need to modify your theme used if it has no built in support already. 
+ * 
+ * Usage examples:
+ * 
+ * a) Object model 
+ * $featuredimage = getFeaturedImage(<object of the Zenpage item>);
+ * if($featuredimage) { // if an feature image exists use the object model
+ *  ?>
+ *  <img src="<?php echo pathurlencode($featuredimage->getThumb()); ?>" alt="<?php echo html_encode($featuredimage->getTitle()); ?>">
+ *  <?php
  * }
+ * 
+ * b) Theme function for pages.php and news.php
+ * <?php printSizedFeaturedImage('My featured image',500); ?>
+ *  
  * Requirement: Zenpage CMS plugin and a theme supporting it
  *
  * @author Malte Müller (acrylian)
@@ -17,127 +27,371 @@
  * @subpackage misc
  */
 $plugin_is_filter = 5|ADMIN_PLUGIN|THEME_PLUGIN;
-$plugin_description = gettext("Attach an album thumb to an news article.");
+$plugin_description = gettext("Attach an image to a Zenpage news article, category or page.");
 $plugin_author = "Malte Müller (acrylian)";
-$plugin_version = '1.4.4';
+$plugin_version = '1.1.0';
 $plugin_disable = (!getOption('zp_plugin_zenpage'))?gettext('The Zenpage CMS plugin is required for this and not enabled!'):false;
 if(getOption('zp_plugin_zenpage')) {
-		zp_register_filter('publish_article_utilities','getFeaturedImageSelector');
-		zp_register_filter('new_article','saveFeaturedImageSelection');
-		zp_register_filter('update_article','saveFeaturedImageSelection');
-		zp_register_filter('remove_object','deleteFeaturedImage');
+  zp_register_filter('publish_article_utilities','featuredImage::getFeaturedImageSelector');
+  zp_register_filter('publish_page_utilities','featuredImage::getFeaturedImageSelector');
+  zp_register_filter('publish_category_utilities','featuredImage::getFeaturedImageSelector');
+	zp_register_filter('remove_object','featuredImage::deleteFeaturedImage');
+	zp_register_filter('admin_head', 'featuredImage::featuredImageCSS');
 }
 
 /*************************
 * Backend functions 
-**************************
+**************************/
 
-/**
- * gets a dropdown selector with all toplevel albums whose album thumb you can assign to an article
- *
- * @param string $html
- * @param object $obj Object of the item to assign the thumb
- * @param string $prefix (not used)
- * @return string
- */
-function getFeaturedImageSelector($html, $obj, $prefix='') {
-	$html .= '<hr /><p>Select album for thumb attachement</p>';
-	$html .= '<select size="1" style="width: 180px" id="featuredimage" name="featuredimage">';
-	$selection = getFeaturedImageSelection($obj);
-	$noneselected = ' selected="selected"';
-	if(!$selection || $selection == 'none') { 
-		$noneselected = '';
+class featuredImage {
+
+	static function featuredImageCSS() {
+		?>
+		<link rel="stylesheet" type="text/css" href="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/featured_image/style.css" />
+		<?php
 	}
-	$html .= '<option'.$noneselected.' value="none">None</option>';
-	$gallery = new Gallery();
-	if($gallery->getNumAlbums() != 0) {
-		echo "test";
-		$albums = $gallery->getAlbums(0);
+
+	/**
+	 * gets a dropdown selector with all toplevel albums whose album thumb you can assign to an article
+	 *
+	 * @param string $html
+	 * @param object $obj Zenpage article, category or page object to assign the thumb
+	 * @param string $prefix (not used)
+	 * @return string
+	 */
+	static function getFeaturedImageSelector($html, $obj, $prefix='') {
+		global $_zp_gallery;
+		$selection = featuredImage::getFeaturedImageSelection($obj);
+		if($selection) {
+			$buttontext = gettext('Change');
+		} else {
+			$buttontext = gettext('Set');
+		}
+		$itemtype = featuredImage::getFeaturedImageType($obj);
+		switch($itemtype) {
+			case 'featuredimage_article':
+				$type = 'news'; // this is needed for the getItemByID() function using in Ajax calls
+				$itemid = $obj->getID();
+				break;
+			case 'featuredimage_category':
+				$type = 'news_categories';
+				$itemid = $obj->getID();
+				break;
+			case 'featuredimage_page': 
+				$type = 'pages';
+				$itemid = $obj->getID();
+				break;
+		}
+		// admin utility box button to call the dialog
+		$html .= '<hr /><h3>Feature image</h3>';
+		$fimage = featuredImage::getFeaturedImage($obj);
+		$imghtml = '';
+		if($fimage) {
+			$imghtml = '<img src="'.html_encode($fimage->getThumb()).'" alt="">';
+		}
+		$html .= '<div id="fi_adminthumb"><a href="#" class="fi_opener">'.$imghtml.'</a></div>';
+		$html .= '<p class="buttons fi_adminbuttons"><button class="fi_opener fi_opener_admin">'.$buttontext.'</button>';
+    $html .= '</p>';
+		// this is part of the dialog window
+		$albumshtml = '<ul>';
+		$albumshtml .= '<li><a href="#" class="active fi_image">'.gettext('Current featured image').'</a></li>';
+		$albums = $_zp_gallery->getAlbums();
 		foreach($albums as $album) {
-			$albobj = new Album($gallery,$album);
-			$selected = '';
-			if($selection == $album) { 
-				$selected = ' selected="selected"';
+			$albobj = newAlbum($album);
+			if($albobj->getNumImages() == 0) {
+				$albumshtml .= '<li>'.$albobj->getTitle().' <small>('.$albobj->getNumImages().')</small>';
+			} else {
+				$albumshtml .= '<li><a href="#" title="'.$albobj->getID().'" class="fi_album">'.$albobj->getTitle().'</a> <small>('.$albobj->getNumImages().')</small>';
 			}
-			$html .= '<option'.$selected.' value="'.$album.'">'.pathurlencode($albobj->getTitle()).'</option>';
+			$albumshtml .= featuredImage::getFeaturedImageSubalbums($albobj);
+			$albumshtml .= '</li>';
+		}
+		$albumshtml .= '</ul>';
+		$html .= '
+			<div id="featuredimageselector">
+				<div id="fi_albumlist">'.$albumshtml.'</div>
+				<div id="fi_content"><h4>'.gettext('Current featured image').'</h4></div>
+			</div>';
+		$html .= '<script>
+		$(document).ready(function(){
+			var winwidth = $(window).width();
+			var winheight = $(window).height();
+			if(winwidth-150 != 0) {
+				winwidth = winwidth-150;
+			}
+			if(winheight-150 != 0) {
+				winheight = winheight-150;
+			}
+			$( "#featuredimageselector" ).dialog({
+				autoOpen: false,
+				modal: true,
+				resizable: true,
+				title: "Select a featured image",
+				closeOnEscape: true,
+				width: winwidth,
+				height: winheight
+			});
+ 
+			$( ".fi_opener" ).click(function() {
+				$( "#featuredimageselector" ).dialog( "open" );
+				$( "#featuredimageselector #fi_content" ).html("");
+				$( "#featuredimageselector #fi_content" ).load( "'.WEBPATH.'/'.USER_PLUGIN_FOLDER.'/featured_image/ajax.php?featuredimage='.$itemid.'&type='.$type.'");
+			
+				//current featured image
+				$( "#featuredimageselector #fi_albumlist li a.fi_image" ).click(function() {
+					var linktitle = $(this).attr( "title" );
+					$("#fi_albumlist li a").removeClass( "active" );
+					$(this).addClass( "active" );
+					$( "#featuredimageselector #fi_content" ).load( "'.WEBPATH.'/'.USER_PLUGIN_FOLDER.'/featured_image/ajax.php?featuredimage='.$itemid.'&type='.$type.'");
+				});
+			
+				// thumbs of an album
+				$( "#featuredimageselector #fi_albumlist li a.fi_album" ).click(function() {
+					var id = $(this).attr( "title" );
+					$("#fi_albumlist li a").removeClass( "active" );
+					$(this).addClass( "active" );
+					$( "#featuredimageselector #fi_content" ).load( "'.WEBPATH.'/'.USER_PLUGIN_FOLDER.'/featured_image/ajax.php?getalb="+id+"&itemid='.$itemid.'&type='.$type.'&imgpage=1" );
+				});       
+				return false;
+			});
+     
+		});
+		</script>';
+		return $html;
+	}
+	
+	/**
+	 * Gets the subalbums of the album object $albobj recursively
+	 * @param object $albobj album object
+	 */
+	private static function getFeaturedImageSubalbums(&$albobj) {
+		if($albobj->getNumAlbums() != 0) {
+			$html = '<ul>';
+			$albums = $albobj->getAlbums();
+			foreach($albums as $album) {
+				$obj = newAlbum($album);
+				if($obj->getNumImages() == 0) {
+					$html .= '<li>'.$obj->getTitle().' <small>('.$obj->getNumImages().')</small>';
+				} else {
+					$html .= '<li><a href="#" title="'.$obj->getID().'" class="fi_album">'.$obj->getTitle().'</a> <small>('.$obj->getNumImages().')</small>';
+				}
+				$html .= featuredImage::getFeaturedImageSubalbums($obj);
+				$html .= '</li>';
+			}
+			$html .= '</ul>';
+			return $html;
 		}
 	}
-	$html .= '</select>';
-	return $html;
-}
 
-/**
- * Gets the album selection of this article
- * @param object $obj Object of the item to assign the thumb
- */
-function getFeaturedImageSelection($obj) {
-	$aux = $obj->getID();
-	$query = query_single_row("SELECT `data` FROM ".prefix('plugin_storage')." WHERE `type` = ".db_quote('featuredimage_article')." AND `aux` = ".db_quote($aux));
-	if($query) {
-		return $query['data'];
-	} else {
+	/**
+	 * Gets the album selection of this article
+	 * @param object $obj Zenpage article, category or page object to assign the thumb
+	 */
+	static function getFeaturedImageSelection($obj) {
+		$type = featuredImage::getFeaturedImageType($obj);
+		if($type) {
+			$itemid = $obj->getID();
+			$query = query_single_row("SELECT `data` FROM ".prefix('plugin_storage')." WHERE `type` = ".db_quote($type)." AND `aux` = ".db_quote($itemid));
+			if($query) {
+				return $query['data'];
+			} else {
+				return false;
+			}
+		}
 		return false;
 	}
-}
 
-/**
- * Saves the album selection of this article
- * @param string $message Message (returns empty or an error)
- * @param object $obj Object of the item to assign the thumb
- */
-function saveFeaturedImageSelection($message,$obj) {
-	if(isset($_POST['featuredimage'])) {
-		$type = 'featuredimage_article';
-		$data = sanitize($_POST['featuredimage']);
-		$aux = $obj->getID();
-		$query = query_single_row("SELECT `data` FROM ".prefix('plugin_storage')." WHERE `type` = ".db_quote($type)." AND `aux` = ".db_quote($aux));
-		if($query) {
-			$query = query("UPDATE ".prefix('plugin_storage')." SET `data` = ".db_quote($data)." WHERE `type` = ".db_quote($type)." AND `aux` = ".db_quote($aux));
+	/**
+	 * Saves the album selection of this article
+	 * @param object $obj Zenpage article, category or page object to assign the thumb
+	 */
+	static function saveFeaturedImageSelection($obj) {
+		$type = featuredImage::getFeaturedImageType($obj);
+		$message = '';
+		if(isset($_GET['imgid']) && $type) {
+			$data = sanitize_numeric($_GET['imgid']);
+			$itemid = $obj->getID();
+			$query = query_single_row("SELECT `data` FROM ".prefix('plugin_storage')." WHERE `type` = ".db_quote($type)." AND `aux` = ".db_quote($itemid));
+			if($query) {
+				$query = query("UPDATE ".prefix('plugin_storage')." SET `data` = ".db_quote($data)." WHERE `type` = ".db_quote($type)." AND `aux` = ".db_quote($itemid ));
+			} else {
+				$query = query("INSERT INTO ".prefix('plugin_storage')." (`type`,`data`,`aux`) VALUES (".db_quote($type).",".db_quote($data).",".db_quote($itemid ).")");
+			}
+			if (!$query) {
+				$message .= '<p class="errorbox">'.sprintf(gettext('Query failure: %s'),db_error()).'</p>';
+			}
+		} 
+		return $message;
+	}
+	
+	/**
+	 * Removes albumthumb as entries from plugin_storage table if their object is removed.
+	 *
+	 * @param object $obj Zenpage article, category or page object being removed
+	 * @return bool
+	 */
+	static function deleteFeaturedImage($obj) {
+		$type = featuredImage::getFeaturedImageType($obj);
+		if($type) {
+			$itemid = $obj->getID();
+			$check = query_single_row("SELECT `data` FROM ".prefix('plugin_storage')." WHERE `type` = ".db_quote($type)." AND `aux` = ".db_quote($itemid));
+			if($check) {
+				$query = query('DELETE FROM '.prefix('plugin_storage').' WHERE `aux` = '.db_quote($itemid).' AND `type` = '.db_quote($type).'',false);
+			}
+		}
+	}
+	
+	/**
+	 * Checks the class of $obj and returns the Zenpage item type "featuredimage_article/category/page"
+	 * Returns false if no Zenpage object is passed
+	 *
+	 * @param object $obj Zenpage article, category or page object
+	 * @return mixed
+	 */
+	static function getFeaturedImageType($obj) {
+		if(is_object($obj)) {
+			switch(get_class($obj)) {
+				case 'ZenpageNews':
+					return 'featuredimage_article'; 
+				case 'ZenpageCategory':
+					return 'featuredimage_category';
+				case 'ZenpagePage':
+					return 'featuredimage_page';
+				default: 
+					return false;
+			}
+		} 
+	}
+	/**
+	 * Function to be used in the dialog box for displaying the single image 
+	 * for setting the featured image and the current featured image 
+	 *
+	 * @param object $obj the object of the image
+	 * @param string $buttontype "save" for save button and "delete" for delete button
+	 */
+	static function printFeaturedImageDialog($imgobj,$buttontype) {
+		$album = $imgobj->getAlbum();
+		if (isImagePhoto($imgobj)) {
+			$url = pathurlencode($imgobj->getSizedImage(400));
 		} else {
-			$query = query("INSERT INTO ".prefix('plugin_storage')." (`type`,`data`,`aux`) VALUES (".db_quote($type).",".db_quote($data).",".db_quote($aux).")");
+			$url = pathurlencode($imgobj->getThumb());
 		}
-		if (!$query) {
-			$message .= '<p class="errorbox">'.sprintf(gettext('Query failure: %s'),db_error()).'</p>';
-		}
-	} 
-	return $message;
-}
+		?>
+		<div id="fi_imagepreview">
+			<img src="<?php echo $url; ?>" alt="<?php echo html_encode($imgobj->getTitle()); ?>" class="fi_thumb">
+		</div>
+		<div id="fi_imageinfo">
+			<h5><?php echo html_encode($imgobj->getTitle().' ('.$imgobj->filename.')'); ?></h5>
+			<p><?php echo gettext('Album: '); ?><?php echo html_encode($album->getTitle().' ('.$album->name.')'); ?></p>
+			<?php 
+				switch($buttontype) {
+					case 'save':
+						$buttontext = gettext('Set feature image');
+						$buttonid = 'fi_setimage';
+						break;
+					case 'delete':
+						$buttontext = gettext('Remove feature image');
+						$buttonid = 'fi_deleteimage';
+						break;
+				}
+				?>
+			<p class="buttons"><button id="<?php echo $buttonid; ?>"><?php echo $buttontext; ?></button></p>
+    	<br class="clearfix">
+		</div>
+		<?php
+	}
 
-/**
- * Removes albumthumb as entries from plugin_storage table if their object is removed.
- *
- * @param bool $allow we just return this since we have no need to abort the remove
- * @param object $obj the object being removed
- * @return bool
- */
-function deleteFeaturedImage($allow,$obj) {
-	if(get_class($obj) == 'ZenpageNews') {
-		$check = query_single_row("SELECT `data` FROM ".prefix('plugin_storage')." WHERE `type` = 'featuredimage_article' AND `aux` = ".$obj->getID());
-		if($check) {
-			$query = query('DELETE FROM '.prefix('plugin_storage').' WHERE `aux` = '.$obj->getID().' AND `type` = "featuredimage_article"',false);
+	/**
+	 * Returns the album thumb image object if one assigned
+	 *
+	 * @param obj/strong $obj Zenpage article, category or page object you want the featured image of
+	 */
+	static function getFeaturedImage($obj) {
+		$imgobj = '';
+		$imagedata = featuredImage::getFeaturedImageSelection($obj);
+		if ($imagedata) {
+			if (is_numeric($imagedata)) {
+				$imgobj = getItemByID('images', $imagedata);
+			} else {
+				//fallback for existing entries using the old albumthumb only way 
+				//where the album name was stored
+				$album = newAlbum($imagedata);
+				$imgobj = $album->getAlbumThumbImage();
+			}
+			if (is_object($imgobj)) {
+				return $imgobj;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
 		}
 	}
-	return $allow;
+
 }
 
-/*************************
-* Frontend theme function
-**************************
+    /* ***********************
+     * Frontend theme function
+     * ***********************/
 
-/**
- * Returns the album thumb image object if one assigned
- *
- * @param ubj $obj news article object you want the featured image
- */
-function getFeaturedImage($obj) {
-	global $_zp_gallery;
-	$album = getFeaturedImageSelection($obj);
-	if(!$album || $album == 'none') {
-		return NULL;
-	} else {
-		$newalbum = newAlbum($album);
-		$albumthumbimg = $newalbum->getAlbumThumbImage();
-		return $albumthumbimg;
-	}
-}
+      /**
+     * Returns the image object if an featured image is assigned, otherwise false
+     *
+     * @param obj/string $obj Object of the Zenpage article, category you want the featured image to get
+     * return mixed
+     */
+    function getFeaturedImage($obj = NULL) {
+      global $_zp_current_zenpage_news, $_zp_current_category, $_zp_current_zenpage_page, $_zp_gallery_page;
+      if (is_null($obj)) {
+        switch ($_zp_gallery_page) {
+          case 'news.php':
+            if (is_object($_zp_current_category) && !is_object($_zp_current_zenpage_news)) {
+              $obj = $_zp_current_category;
+            } else if (is_object($_zp_current_zenpage_news)) {
+              $obj = $_zp_current_zenpage_news;
+            } 
+            break;
+          case 'pages.php':
+            if (is_object($_zp_current_zenpage_page)) {
+              $obj = $_zp_current_zenpage_page;
+            }
+            break;
+        }
+      }
+      $img = featuredImage::getFeaturedImage($obj);
+      return $img;
+    }
+
+ 
+    /**
+     *  Print a custom sized version of this image based on the parameters. Multimedia itemas don't use these.
+     *
+     * @param obj/string $obj Object of the Zenpage article, category you want the featured image to get
+     * @param int $size size
+     * @param int $width width
+     * @param int $height height
+     * @param int $cropw crop width
+     * @param int $croph crop height
+     * @param int $cropx crop x axis
+     * @param int $cropy crop y axis
+     * @param bool $thumbStandin set to true to treat as thumbnail
+     * @param bool $effects set to desired image effect (e.g. 'gray' to force gray scale)
+     * @return string
+     */
+    function printSizedFeaturedImage($obj = NULL,$alt, $size, $width = NULL, $height = NULL, $cropw = NULL, $croph = NULL, $cropx = NULL, $cropy = NULL, $class = NULL, $id = NULL, $thumbStandin = false, $effects = NULL) {
+      $image = getFeaturedImage($obj);
+      if ($image) {
+        if(isImagePhoto($image)) {
+          if(empty($alt)) $alt = $image->getTitle();
+          if(!empty($class)) $class = ' class="'.$class.'"';
+          if(!empty($id)) $id = ' id="'.$id.'"';
+          $url = $image->getCustomImage($size, $width, $height, $cropw, $croph, $cropx, $cropy, $thumbStandin, $effects);
+          ?>
+          <img src="<?php echo pathurlencode($url); ?>"<?php echo $class.$id; ?> alt="<?php echo html_encode($alt); ?>">
+          <?php
+        } else {
+          echo $image->getBody();
+        }
+      }
+    }
 ?>
---
+++ featured_image/ajax.php
@@ -0,0 +1,188 @@
+<?php 
+require_once(dirname(dirname(dirname(__FILE__)))."/zp-core/admin-globals.php");
+admin_securityChecks(ZENPAGE_PAGES_RIGHTS | ZENPAGE_NEWS_RIGHTS, '');
+require_once(SERVERPATH.'/'.ZENFOLDER.'/'.PLUGIN_FOLDER."/zenpage/zenpage-template-functions.php");
+ 
+// current featured image
+if (isset($_GET['featuredimage'])) {
+	$type = sanitize( $_GET['type'] );
+	$id = sanitize_numeric($_GET['featuredimage']);
+  $obj = getItemByID($type, $id);
+  $title = $obj->getTitle();
+  $image = featuredImage::getFeaturedImage($obj);
+  ?>
+  <h4><?php echo gettext('Current featured image for '); echo '<em>'.html_encode($title).' ('.$type.')</em>'; ?></h4>
+  <p class="notebox"><?php echo gettext('<strong>Note:</strong> Your theme might need changes to support this feature.'); ?></p>
+  <div id="fi_singleimage">
+  <?php
+  if ($image) {
+    featuredImage::printFeaturedImageDialog($image,'delete');
+    ?>
+    <script>
+			$(document).ready(function(){
+				$( "#fi_deleteimage" ).click(function() {
+          $.ajax({
+          	type: "GET",
+  					url: "<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/featured_image/ajax.php",
+  					data: { 
+  						deletefeaturedimage: "<?php echo $id; ?>", 
+  						type: "<?php echo $type; ?>"
+  					}
+          }); 
+          $( "#fi_adminthumb a" ).html( '' );
+          $(".fi_opener_admin").html("<?php echo gettext('Set'); ?>");
+          $( "#featuredimageselector" ).dialog( "close" );
+        });
+		  }); 
+		</script>
+    <?php
+  } else {
+    ?>
+    <p><?php echo gettext('No featured image selected.'); ?></p>
+    <?php
+  }
+  ?>
+  </div>
+  <?php
+}
+
+// Set featured image
+if (isset($_GET['savefeaturedimage'])) {
+	$type = sanitize( $_GET['type'] );
+	$itemid = sanitize_numeric( $_GET['savefeaturedimage'] );
+	$obj = getItemByID($type, $itemid);
+	$message = featuredImage::saveFeaturedImageSelection($obj);
+}
+
+// Remove featured image
+if (isset($_GET['deletefeaturedimage'])) {
+	$type = sanitize( $_GET['type'] );
+	$itemid = sanitize_numeric( $_GET['deletefeaturedimage'] );
+	$obj = getItemByID($type, $itemid);
+	featuredImage::deleteFeaturedImage($obj);
+}
+
+// single image display for preview and setting featured image
+if(isset($_GET['getimg'])) {
+	$type = sanitize( $_GET['type'] );
+	$id = sanitize_numeric( $_GET['getimg'] );
+	$itemid = sanitize_numeric( $_GET['itemid'] );
+	if(!empty($id) && !empty($itemid)) {
+		$image = getItemByID('images', $id);
+    featuredImage::printFeaturedImageDialog($image,'save');
+    ?>
+    <script>
+			$(document).ready(function(){
+				$( "#fi_setimage" ).click(function() {
+          $.ajax({
+          	type: "GET",
+  					url: "<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/featured_image/ajax.php",
+  					data: { 
+  						savefeaturedimage: "<?php echo $itemid; ?>", 
+  						imgid: "<?php echo $id;?>",
+  						type: "<?php echo $type; ?>"
+  					}
+          });
+          $( "#fi_adminthumb a" ).html( '<img src="<?php echo pathurlencode($image->getThumb()); ?>" alt="">' );
+          $(".fi_opener_admin").html("<?php echo gettext('Change'); ?>");
+          $( "#fi_singleimage" ).dialog( "close" );
+        });
+		  }); 
+		</script>
+    <?php
+	} else {
+    ?>
+    Sorry…
+    <?php
+  }
+} 
+
+// image thumbs of an album
+if(isset($_GET['getalb']) && isset($_GET['imgpage'])) {
+	$type = sanitize( $_GET['type'] );
+	$itemid = sanitize_numeric( $_GET['itemid'] );
+	$id = sanitize_numeric( $_GET['getalb'] );
+	$imgpage = sanitize_numeric( $_GET['imgpage'] );
+	if(!empty($id) && !empty($imgpage) && !empty($itemid)) {
+		$obj = getItemByID('albums', $id);
+		$images = $obj->getImages($imgpage); 
+		$numimages = $obj->getNumImages();
+		$img_per_page = getOption('images_per_page');
+		$totalpages = ceil($numimages / $img_per_page);
+		?>
+		<script>
+			$(document).ready(function(){
+				$(".fi_pagenav").html(''); //clear the html so the nav is always fresh!
+				var total = <?php echo $totalpages; ?>;
+				var current = <?php echo $imgpage; ?>;
+				var activeclass = '';
+				if(total > 1) {
+					if(current !== 1) {
+						$(".fi_pagenav").append('<li><a href="#" title="'+(current-1)+'">prev</a></li>');
+					}
+					for (i = 1; i <= total; i++) {
+						if(current == i) {
+							activeclass = ' class = "active"';
+						} else {
+							activeclass = '';
+						}
+						$(".fi_pagenav").append('<li><a href="#" title="'+i+'"'+activeclass+'>'+i+'</a></li>');
+					}
+					if(current < total) {
+						$(".fi_pagenav").append('<li><a href="#" title="'+(current+1)+'">next</a></li>');
+					}
+				}
+				$( ".fi_pagenav li a" ).click(function() {
+					var imgpage = $(this).attr( "title" );
+					//alert("click");
+					$( "#featuredimageselector #fi_content" ).load( "<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/featured_image/ajax.php?getalb=<?php echo $id; ?>&itemid=<?php echo $itemid; ?>&type=<?php echo $type; ?>&imgpage="+imgpage);
+				});
+				
+        //single image dialog to preview uncropped and set feature image
+        $( "#fi_singleimage" ).dialog({
+          autoOpen: false,
+          modal: true,
+          resizable: true,
+          closeOnEscape: true,
+          width: 640,
+          height: 480
+        });
+        
+        //dialog to preview and set an image
+        $( "#featuredimageselector #fi_content a.fi_thumb" ).click(function() {
+          $( "#fi_singleimage" ).html( "" );
+          $( "#fi_singleimage" ).dialog( "open" );
+          var id = $('img',this).attr( "title" );
+          $( "#fi_singleimage" ).load( "<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/featured_image/ajax.php?getimg="+id+"&itemid=<?php echo $itemid; ?>&type=<?php echo $type; ?>");
+        });
+			});
+		</script>
+		<h4><?php echo $obj->getTitle(); ?> (<?php echo $obj->name; ?>) – <?php echo $obj->getNumImages(); ?></h4>
+		<ul class="fi_pagenav"></ul><!-- filled with the nav -->
+		<?php
+		if($numimages == 0) {
+			?>
+			<p class="notebox"><?php echo gettext('This album does not contain any images.'); ?></p>
+			<?php
+		} else {
+			foreach($images as $image) {
+				$imgobj = newImage($obj, $image);
+        if (isImagePhoto($imgobj)) {
+          $extraclass = " imagetype";
+        } else {
+          $extraclass = " nonimagetype";
+        }
+        ?>
+				<a href="#" title="<?php echo html_encode($imgobj->getTitle().' ('.$imgobj->filename.')'); ?>" class="fi_thumb<?php echo $extraclass;?>">
+          <img src="<?php echo pathurlencode($imgobj->getThumb()); ?>" alt="<?php echo html_encode($imgobj->getTitle().' ('.$imgobj->filename.')'); ?>" title="<?php echo $imgobj->getID(); ?>">
+				</a>
+				<?php
+			}
+      ?>
+      <ul class="fi_pagenav"></ul><!-- filled with the nav -->
+      <div id="fi_singleimage"></div>
+      <?php
+		}
+	} // if variables not empty
+} 
+?>
